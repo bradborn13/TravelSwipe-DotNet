947b19f454065ab5521f4f38a1cf0a4cca1fa196
Lucian <Lucian@LUCIANRIG>
2026-03-14 22:26:32 +0100

    migration fix

---
+++ Countries.Infrastructure/Migrations/20260314212420_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Countries.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    associated_names = table.Column<List<string>>(type: "text[]", nullable: true),
+                    associated_slugs = table.Column<List<string>>(type: "text[]", nullable: true),
+                    display_name = table.Column<string>(type: "text", nullable: false),
+                    country_code = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_country", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_country_display_name",
+                table: "Country",
+                column: "display_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
